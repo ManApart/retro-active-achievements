cf39ad79ec492f7118a7724c09aa5e45060cc3b1
ManApart <raustinkucera@gmail.com>
2019-12-23 21:29:27 -0500

    working

--- RetroActiveAchievements/MainModClass.cs
+++ RetroActiveAchievements/MainModClass.cs
@@ -2,47 +2,37 @@
 
 using StardewModdingAPI;
 using StardewValley;
-using StardewValley.Network;
-using Microsoft.Xna.Framework.Net;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using StardewValley.Menus;
+
+using Steamworks;
 
 namespace ShipmentTrackerSMAPI {
 
     public class MainModClass : Mod {
         private static MainModClass instance;
 
-      private String statsFilePath;
-        private int lastDayRun = -1;
-        private List<Item> itemsToShip;
-
-
         public override void Entry(IModHelper helper) {
            instance = this;
-            statsFilePath = helper.DirectoryPath + Path.DirectorySeparatorChar;
-            helper.Events.GameLoop.Saved += AfterSaveEvent;
-            helper.Events.GameLoop.OneSecondUpdateTicking += GameEvents_OneSecondTick;
-
-            //also maybe could use:
-            //GameEvents.Events.TimeEvents.DayOfMonthChanged
+            helper.Events.GameLoop.SaveLoaded += OnLoad;
 
             Log("RetroActive Achievement Loader by Iceburg333 => Initialized");
         }
 
-        private void AfterSaveEvent(object sender, EventArgs e) {            
-            Log("after save");
+        private void OnLoad(object sender, EventArgs e) {            
+            Log("Player has " + Game1.player.achievements.Count + " achievements");
+            Log("Steam API is running? " + SteamAPI.IsSteamRunning());
+
+            foreach (int achievement in Game1.player.achievements)
+            {
+                string achievementName = Game1.achievements[achievement];
+                Log("Player has achievement " + achievement + ": " + achievementName);
+                Program.sdk.GetAchievement(string.Concat((object)achievement));
+            }
+            
+            Program.sdk.ResetAchievements();
+            Game1.playSound("achievement");
         }
 
-        private void GameEvents_OneSecondTick(object sender, EventArgs e) {
-            if (!Game1.hasLoadedGame) { 
-                return;
-            }
-            Log("On Second Tick");
-          
-        }
-    
         public static void Log(string message) {
             instance.Monitor.Log(message, LogLevel.Debug);
         }
